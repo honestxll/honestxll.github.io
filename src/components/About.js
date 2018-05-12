c82b9852f08bd70622da4070807f76061956c461
13451883267 <1131328108@qq.com>
2018-05-12 15:14:58 +0800

    关于我的页面

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,91 @@
 define(require => {
   const Menu = require('components/Menu');
   return {
+    mounted() {
+      $('.ui.progress')
+        .progress({
+          showActivity: false,
+        })
+    },
     template: `
       <div class="pager">
         <Menu></Menu>
-      </div>
+        <div class="ui about">
+          <div class="left section">
+            <div class="top banner image">
+              <img src="http://demo.cssmoban.com/cssthemes3/btst_33_readonly/images/banner.jpg" alt=""/>
+            </div>
+            <div class="ui myContainer">
+              <h2 class="header">What can I do</h2>
+              <div class="ui stack two column grid">
+                <div class="column">
+                  <div class="ui small indicating progress" data-percent="80" data-total="100">
+                    <h3 class="stack">PHP</h3>
+                    <div class="bar">
+                      <div class="progress"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="column">
+                  <div class="ui small indicating progress" data-percent="60" data-total="100">
+                    <h3 class="stack">Node.js</h3>
+                    <div class="bar">
+                      <div class="progress"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="column">
+                  <div class="ui small indicating progress" data-percent="80" data-total="100">
+                    <h3 class="stack">H5</h3>
+                    <div class="bar">
+                      <div class="progress"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="column">
+                  <div class="ui small indicating progress" data-percent="80" data-total="100">
+                    <h3 class="stack">CSS3</h3>
+                    <div class="bar">
+                      <div class="progress"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="column">
+                  <div class="ui small indicating progress" data-percent="60" data-total="100">
+                    <h3 class="stack">React Native</h3>
+                    <div class="bar">
+                      <div class="progress"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="column">
+                  <div class="ui small indicating progress" data-percent="10" data-total="100">
+                    <h3 class="stack">Java</h3>
+                    <div class="bar">
+                      <div class="progress"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="right section">
+            <div class="ui info">
+              <div class="image">
+                <img src="http://demo.cssmoban.com/cssthemes3/btst_33_readonly/images/avatar.jpg" alt=""/>
+              </div>
+              <h3 class="header">Honest</h3>
+              <div class="content">化身奶爸...</div>
+            </div>
+            <div class="ui contact">
+              <h4 class="ui horizontal divider header">
+                <i class="mail icon"></i>
+                邮箱
+              </h4>
+              <p class="center aligned">honestxll@gmail.com</p>
+            </div>
+          </div>
+        </div>
     `,
     components: {
       Menu,
