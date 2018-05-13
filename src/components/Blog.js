314924d875f8444cdcd682ebb2537332d4328c03
13451883267 <1131328108@qq.com>
2018-05-13 18:13:44 +0800

    博客列表

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,9 @@
 define(function(require) {
+  const ScrollReveal = require('bower_components/scrollreveal/dist/scrollreveal.min')
+  const Moment = require('bower_components/moment/min/moment-with-locales.min')
+
+  const sr = ScrollReveal({ reset: true })
+
   return {
     data() {
       return {
@@ -12,14 +17,19 @@ define(function(require) {
             .prev('input')
             .toggleClass('reveal')
         })
+      sr.reveal('.ui.blog.container', 100)
     },
     created() {
       fetch('/md/config.json')
         .then(response => response.json())
         .then(responseData => {
           this.files = responseData.files
-          console.log(this.files);
         })
+    },
+    filters: {
+      moment(time) {
+        return Moment(time).locale('zh-cn').startOf('hour').fromNow()
+      }
     },
     template: `
       <div class="blog pager">
@@ -28,6 +38,18 @@ define(function(require) {
             <input type="text" placeholder="搜索...">
             <i class="search icon"></i>
           </div>
+        </div>
+        <div class="ui blog container">
+          <ol class="ui articles">
+            <router-link
+              class="article"
+              v-for="(file, index) in files"
+              :to="'/blog/'+index"
+              tag="li">
+              <div class="title">{{ file.name }}</div>
+              <span class="time">{{ file.time | moment }}</span>
+            </router-link>
+          </ol>
         </div>
       </div>
     `
