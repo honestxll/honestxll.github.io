de60606aceaf0167484ad7820ed1f8d1fe7e2030
13451883267 <1131328108@qq.com>
2018-05-13 18:14:51 +0800

    添加了几个插件的缩写

--- src/config.js
+++ src/config.js
@@ -4,15 +4,28 @@ require.config({
     jquery: 'bower_components/jquery/dist/jquery.min',
     vue: 'bower_components/vue/dist/vue.min',
     vueRouter: 'bower_components/vue-router/dist/vue-router.min',
-    semantic: 'bower_components/semantic/dist/semantic.min'
+    semantic: 'bower_components/semantic/dist/semantic.min',
+    highlight: 'bower_components/highlightjs/highlight.pack.min',
+    md: 'bower_components/markdown-it/dist/markdown-it.min',
   },
   shim: {
-    semantic: ['jquery']
+    semantic: ['jquery'],
+    highlight: {
+      exports: 'hljs'
+    },
+    html: {
+      exports: 'html'
+    }
   },
   packages: {
     name: 'components',
     location: 'components',
     name: 'components',
+  },
+  packages: {
+    name: 'bower_components',
+    location: 'bower_components',
+    name: 'bower_components',
   }
 })
 require(['./main'])
